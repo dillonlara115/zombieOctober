42102f0ab9e5b3647475808de6eeaab42f08e7ff
Dillon <dillonlara115@me.com>
2013-10-03 15:43:58 -0500

    init commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+﻿// Variables
+var mobileNav = $('nav'); //navigation menu variable
+
+//mobileNav.hide();
+//Show small screen navigation menu when navigation button is pressed
+$('.navbar').on('click', 'button', function () {
+    $('header').animate({
+        top: '-=130px'
+    });
+
+    mobileNav.delay(100).animate({
+        left: '+=260px'
+    }, 700); //toggles the menu display
+});
+
+$('.is-open').on('click', 'button', function () {
+    mobileNav.animate({
+        left: '-=260px'
+    }, 700); //toggles the menu display
+    $('header').delay(300).animate({
+        top: '+=130px'
+    });
+});
+
